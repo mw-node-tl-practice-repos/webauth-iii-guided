5c91a58205734192f49a8f9643a9f4cfa8628dfd
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-10-16 09:46:24 -0700

    Doing webauth III stuff

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,8 +26,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // produce a token here!
+        const token = generateToken(user)
+        // add token to response!
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -37,4 +42,18 @@ router.post('/login', (req, res) => {
     });
 });
 
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+    subject: user.id,
+  };
+
+  const secret = "Secret words go here!";
+
+  const options = {
+    expiresIn: '1h'
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 module.exports = router;
